e8af0b156838d0abcedc9f48add0efa740c0247d
E.Bousk <e.bousk@gmail.com>
2021-11-28 13:46:56 +0100

    Handling GET requests

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// On lit le fichier et on le convertit en tableau d'objet javascript
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// on l'envoie au client (sur la route "api/v1/tours" en méthode GET)
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',            // on spécifie le statut
+    results: tours.length,        // pour renvoyer le nombre d'occurences
+    data: {
+      tours                       // équivalent à « tours: tours »
+    }
+  });
 });
 
 const port = 3000;
