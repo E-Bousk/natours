f9b900356ac97373b92fdcf1adbe2dbf0e116979
E.Bousk <e.bousk@gmail.com>
2021-12-01 22:45:59 +0100

    Deleting documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -57,11 +57,6 @@ exports.createTour = async (req, res) => {
 
 exports.updateTour = async (req, res) => {
   try {
-    // 1er argument : l'ID (vient des paramètres, depuis l'URL)
-    // 2eme argument : les données (viennent de la requête PATCH)
-    // 3eme argument(s) : option(s) - "new" = retourne le document mis à jour plutôt que l'original
-    //                              - "runValidators" valide le document par rapport au model
-    // [https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate]
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
@@ -81,9 +76,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
