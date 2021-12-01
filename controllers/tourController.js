b8ede3e5216104cd469eb3e40e8077b036833d46
E.Bousk <e.bousk@gmail.com>
2021-12-01 18:34:11 +0100

    Creating documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,23 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  console.log('req.body => ', req.body);
-  switch (true) {
-    case !req.body.price:
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Missing price'
-      });
-    case !req.body.name:
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Missing name'
-      });
-    default:
-      next();
-  }
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -31,9 +13,8 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
+  // const id = req.params.id * 1;
   // const tour = tours.find(el => el.id === id);
-
   // res.status(200).json({
   //   status: 'success',
   //   data: {
@@ -42,18 +23,39 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    satus: 'success'
-    // data: {
-    //   tour: newTour
-    // }
-  });
+// On utilise le "async/await" pour traiter la promesse (au lieu de ".then()/.catch")
+// On utilise donc un "try/catch"
+exports.createTour = async (req, res) => {
+  try {
+    // On appelle la méthode "create" directement sur le model lui-même
+    // (Équivalent à :
+    // [« const newTour = new Tour({xxxxx}); »
+    // « newTour.save(); »]
+    // où on appellait la méthode "save" sur le document "newTour" (crée depuis le 'model' "Tour"))
+    // Cette méthode retourne elle aussi une promesse.
+    // On sauvegarde la valeur resultante de la promesse dans la variable "newTour"
+    // et on passe les données de la requête POST ("req.body")
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      // Message avec l'objet "ERR"
+      // message: err
+      message: 'Invalid data sent!'
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
   res.status(200).json({
-    satus: 'success',
+    status: 'success',
     data: {
       tour: '<Updated tour here...>'
     }
@@ -62,7 +64,7 @@ exports.updateTour = (req, res) => {
 
 exports.deleteTour = (req, res) => {
   res.status(204).json({
-    satus: 'success',
+    status: 'success',
     data: null
   });
 };
