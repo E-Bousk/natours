f1e30dc92ea4b40ee0cd73eb140c77a464b23fd3
E.Bousk <e.bousk@gmail.com>
2021-12-08 15:29:59 +0100

    Signing up users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,33 @@
+// On charge « jsonwebtoken »
+const jwt = require('jsonwebtoken');
+
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
-// On 'enveloppe' la méthode 'async/await' dans notre fonction "catchAsync" (qui gère les erreurs)
+// (Avec le token, on va pouvoir connecter l'utilisateur dès qu'il sera enregistré)
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // ‼ En récuperant les données de « req.body », l'utilisateur peut aussi s'ajouter le role « admin » (par exemple)
+  // AVANT : « const newUser = await User.create(req.body); »
+  // Pour éviter cela, on l'enregistre en autorisant seulement les donnnées dont on a besoin ‼
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
+  // Pour créer le token, on passe en arguments :
+  // 1- un 'objet' avec l'ID de l'utilisateur (« payload » = les données qui doivent être stockées dans le token)
+  // 2- un 'string' pour le « secret ou clef privée » (ici récupéré depuis le fichier « config.env »)
+  // 3- en option : une durée de validation (l'(es) option(s) doit(doivent) être passée(s) comme 'objet')
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
 
   res.status(201).json({
     status: 'succes',
+    // On envoie le token créé au client
+    token,
     data: {
       user: newUser
     }
--- models/userModel.js
+++ models/userModel.js
@@ -25,14 +25,8 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
-    // On crée une validation avec une fonction et un message, donc on ouvre un nouvel objet
     validate: {
-      // [NE FONCTIONNE QUE SUR ".create()" ET ".save()" donc si on doit mettre à jour,
-      // il faut utiliser "SAVE" à nouveau et pas "findOneAndUpdate" par exemple.]
-      // On fait une fonction callback appelée quand un nouveau document est crée
-      // NOTE: ‼ pas de fonction flechée pour pouvoir utiliser le mot-clef "this" ‼
       validator: function(el) {
-        // (Si on retourne "faux" on a une erreur de validation)
         return el === this.password;
       },
       message: 'Passwords are not matching!'
@@ -40,18 +34,11 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Mongoose Middleware "pre-save" pour encoder les mots de passe
-// (entre le moment où on reçoit les données et celui où on les inscrit dans la BDD)
 userSchema.pre('save', async function(next) {
-  // uniquement si le mdp change (update) ou s'il est crée (nouveau)
-  // donc si le mdp n'est pas modifié, on sort du middleware
   if (!this.isModified('password')) return next();
 
-  // encode le mdp avec un 'coût' de 12
   this.password = await bcrypt.hash(this.password, 12);
 
-  // On n'a plus besoin de ce champ, et on ne le veut pas dans la BDD
-  // (Note: le "required" fonctionne pour l'input, pas pour le persistement en BDD)
   this.passwordConfirm = undefined;
 
   next();
