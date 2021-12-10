586430468d055b900756a7cf34d37a1855deb7d5
E.Bousk <e.bousk@gmail.com>
2021-12-10 23:15:17 +0100

    Preventing parameter pollution

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-
-// On charge « express-mongo-sanitize »
 const mongoSanitize = require('express-mongo-sanitize');
-
-// On charge « xss-clean »
 const xss = require('xss-clean');
+
+// On charge « hpp » ('HTTP Parameter Pollution')
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -41,14 +40,34 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 
 // ** Data sanitization against NoSQL query injection **
-// Ce middleware nettoie 'req.body', 'req.query' et 'req.params'
-// en filtrant tous les « $ » et « . »
 app.use(mongoSanitize());
 
 // ** Data sanitization against XSS attack **
-// Ce middleware nettoie 'req.body', 'req.query' et 'req.params'
-// remplace les « < » par « &lt; »
 app.use(xss());
+
+// ** Prevent parameter pollution **
+// Note: à utiliser en dernier, car il nettoie la requête ('query string')
+// Exemple : dans Postman, on requête : « api/v1/tours?sort=price&sort=duration » (deux fois « sort »)
+// on avait cette erreur en retour :
+// « this.queryString.sort.split is not a function at APIFeatures.sort (apiFeatures.js:23:44) »
+// car "split" fonctionne avec une chaîne de caractère, pas avec un tableau !!
+// (le console.log donnait « this.queryString.sort = [ 'price', 'duration' ] »)
+// HPP nettoie et ne garde que la dernière valeur. On n'a donc plus d'erreur et on obtient un résultat
+// En revanche, autre exemple : « api/v1/tours?duration=5&duration=9 » (on veut filtrer avec ces 2 valeurs « 5 » et « 9 »)
+// du coup HPP nettoie et ne garde que le « 9 ».
+// Dans ce cas, il faut inscrire ce paramètre dans une 'liste blanche' avec un objet que l'on passe en option
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 // ** Serving static files **
 app.use(express.static(`${__dirname}/public`));
